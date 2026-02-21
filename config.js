894badfa1352b6616b1baaf339e5924232e15012
VikasTammisetty <tammisettyvikas21@gmail.com>
2026-02-21 18:00:27 +0000

    Use environment variable for Firebase API key

---
+++ config.js
@@ -0,0 +1,10 @@
+// Access API key from environment
+const firebaseConfig = {
+  apiKey:   AIzaSyAvB1VYRxCTxc466oAoLGVXLDIo4VuQVYk, // <-- use .env
+  authDomain: "destinova-72b11.firebaseapp.com",
+  projectId: "destinova-72b11",
+  storageBucket: "destinova-72b11.firebasestorage.app",
+  messagingSenderId: "585905994072",
+  appId: "1:585905994072:web:b111649e2ba9b934d30d2d",
+  measurementId: "G-Q2ZMDPYHMY"
+};
--- public/firebase.js
+++ public/firebase.js
@@ -2,16 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
-// Access API key from environment
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,  // <-- use .env
-  authDomain: "destinova-72b11.firebaseapp.com",
-  projectId: "destinova-72b11",
-  storageBucket: "destinova-72b11.firebasestorage.app",
-  messagingSenderId: "585905994072",
-  appId: "1:585905994072:web:b111649e2ba9b934d30d2d",
-  measurementId: "G-Q2ZMDPYHMY"
-};
+import { firebaseConfig } from './config.js';
 
 const app = initializeApp(firebaseConfig);
 
